707080dfb6775f4585fcf3fe9528062ff526d690
Hushcha Dmytriy <dimag586@gmail.com>
2022-02-02 22:09:15 +0200

    SQL Basics init

---
+++ SQL Basics/lesson1.sql
@@ -0,0 +1,11 @@
+SELECT * FROM staff;
+
+SELECT * FROM subject;
+
+SELECT title, publish_year FROM book;
+
+SELECT first_name, last_name, birth_date FROM pupil;
+
+SELECT name, price FROM dish;
+
+SELECT title, number_of_pages FROM book
