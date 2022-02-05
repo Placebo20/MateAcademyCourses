1941012d247fb92a41edabf25fe7d6dc91ba74d2
Hushcha Dmytriy <dimag586@gmail.com>
2022-02-05 19:10:36 +0200

    Added some lessons tasks solutions to SQL Basics pt.2

--- SQL Basics/lesson7.sql
+++ SQL Basics/lesson7.sql
@@ -21,3 +21,42 @@ FROM book
 WHERE publish_year BETWEEN 2011 AND 2015
 GROUP BY publish_year
 ORDER BY publish_year DESC;
+
+SELECT subject, AVG(mark) AS average_mark
+FROM scoreboard
+INNER JOIN pupil
+ON scoreboard.pupil_id = pupil.id
+WHERE first_name = 'Anna' AND last_name = 'Ivaniuk'
+GROUP BY subject
+ORDER BY AVG(mark) DESC;
+
+SELECT name AS department_name, AVG(salary) AS average_salary, COUNT(employee.*) AS number_of_employees
+FROM employee
+INNER JOIN department
+ON employee.department_id = department.id
+GROUP BY department_name;
+
+SELECT first_name, last_name, AVG(mark) AS average_mark
+FROM scoreboard
+INNER JOIN pupil
+ON scoreboard.pupil_id = pupil.id
+GROUP BY first_name, last_name
+ORDER BY pupil.id;
+
+SELECT subject, year, AVG(mark) AS average_mark
+FROM scoreboard
+INNER JOIN pupil
+ON scoreboard.pupil_id = pupil.id
+WHERE birth_date > '2005-01-01'
+GROUP BY subject, year
+ORDER BY subject, year DESC;
+
+SELECT name AS department_name, first_name, last_name
+FROM employee
+RIGHT JOIN department
+ON employee.department_id = department.id;
+
+SELECT name AS department_name, first_name, last_name
+FROM employee
+LEFT JOIN department
+ON employee.department_id = department.id;
